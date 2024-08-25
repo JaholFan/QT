93fad8f9bd754750b699a4ae41d80fe6f9312ebc
jahol <1028795881@qq.com>
2024-08-25 15:47:15 +0800

    UI拖拽设计与槽

---
+++ Generate_Form/mainwindow.cpp
@@ -0,0 +1,26 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    qDebug() << "led灯开";
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    qDebug() << "led灯关";
+}
+
